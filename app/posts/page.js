6064b2222aa535b4961795a7d4fc72428e29e3a6
sumit2456 <124286200+sumit2456@users.noreply.github.com>
2023-09-12 12:05:39 +0530

    Update page.js

--- app/posts/page.js
+++ app/posts/page.js
@@ -12,7 +12,11 @@ export default function Posts() {
       <div className="posts">
         <h1>Post List</h1>
         <ul>
-          {/* Render a list of Links from the posts object   */}
+          {posts.map((e , i) => {
+            return (
+                <li><Link href={e.path}>{e.name}</Link></li>
+            )
+          })}
         </ul>
       </div>
     )
