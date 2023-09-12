a861f0bf16b82d00b211722781aac8c2a2da1020
sumit2456 <124286200+sumit2456@users.noreply.github.com>
2023-09-12 12:05:21 +0530

    Update page.js

--- app/posts/[id]/page.js
+++ app/posts/[id]/page.js
@@ -1,5 +1,12 @@
+'use client'
 import React from 'react';
+import {usePathname} from "next/navigation"
 
 export default function PostDetail({ match }) {
-  // Complete the function
-}
+    const pathname = usePathname()
+    const id = pathname.split("")[pathname.length-1];
+    // console.log();
+    return (
+        <h1>Post Detail: {id}</h1>
+    )
+} 
